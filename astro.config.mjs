54cd10dd551ef5b6885d0a5e44b74ee0ee9d4efc
Andy Summerfield <aj.summerfield93@gmail.com>
2023-02-13 20:15:51 +0000

    Adding environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@ import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
 import { apiPlugin } from "@storyblok/js";
 import netlify from "@astrojs/netlify/functions";
+import { loadEnv } from "vite";
+
+const { CMS_ENV } = loadEnv(import.meta.env.CMS_ENV, process.cwd(), "") || process.env.CMS_ENV;
+const { ACCESS_TOKEN } = loadEnv(import.meta.env.ACCESS_TOKEN, process.cwd(), "") || process.env.ACCESS_TOKEN;
  
 export default defineConfig({
-  output: 'server',
+  output: CMS_ENV === 'preview' ? 'server' : 'static',
+  adapter: CMS_ENV === 'preview' ? netlify() : undefined,
   integrations: [
-    netlify(),
     storyblok({
-      accessToken: "rRfXQMqHfyWxl4AUoL701gtt",
-      bridge: true,
+      accessToken: ACCESS_TOKEN,
+      bridge: CMS_ENV === 'preview',
       components: {
         page: "storyblok/Page",
         hero: "storyblok/Hero",
